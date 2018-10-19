1d0ed4724fbcb8e682a985f7786959307f586ba1
k15024 <k15024kk@aitech.ac.jp>
2018-10-20 04:36:13 +0900

    add md

--- read_bookmarklet_tmp.js
+++ read_bookmarklet_tmp.js
@@ -1,7 +1,7 @@
 javascript:(
     function(d,s){
         s=d.createElement('script');
-        s.src='bookmarklet_path';
+        s.src='https://uchida-kei.github.io/my-bookmarklet/src/test.js';
         d.head.appendChild(s);
     }
 )();
