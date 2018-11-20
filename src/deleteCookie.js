7b7a935a15f56cc79e26e1b56018ee8fc110311d
k15024 <k15024kk@aitech.ac.jp>
2018-11-20 18:33:37 +0900

    change file

--- src/deleteCookie.js
+++ src/deleteCookie.js
@@ -1,4 +1,4 @@
-let tmp = new Date();
-tmp.setTime(0);
-document.cookie = "ya_gh2019_ses=;expires="+tmp.toGMTString();
+//let tmp = new Date();
+//tmp.setTime(0);
+document.cookie = "ya_gh2019_ses=;name=' '"//expires="+tmp.toGMTString();
 location.reload();
